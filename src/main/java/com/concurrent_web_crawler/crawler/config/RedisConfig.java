7546e385ffbcc155d4ba4f2ea1a0d6edea0a7cca
lukeskw <lucasporfirioa@gmail.com>
2025-10-29 09:31:48 -0300

    feat: adding cache configs

--- src/main/java/com/concurrent_web_crawler/crawler/config/RedisConfig.java
+++ src/main/java/com/concurrent_web_crawler/crawler/config/RedisConfig.java
@@ -1,12 +1,22 @@
 package com.concurrent_web_crawler.crawler.config;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.Map;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
@@ -20,5 +30,21 @@ public class RedisConfig {
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory cf) {
         return new StringRedisTemplate(cf);
+    }
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = Map.of(
+                "crawlStateFinal", defaultConfig.entryTtl(Duration.ofMinutes(10))
+        );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
     }
 }
